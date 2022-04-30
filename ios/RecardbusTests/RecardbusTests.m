422dcc48cd678e9e1422ceca493cd77fd62c2fb2
Lucas Dev React Native <lucasdevreactnative@Mac-Pro-de-Lucas.local>
2022-04-30 10:02:54 -0300

    mudando nome do app

--- ios/RecardbusTests/RecardbusTests.m
+++ ios/RecardbusTests/RecardbusTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface previsao_tempoTests : XCTestCase
+@interface RecardbusTests : XCTestCase
 
 @end
 
-@implementation previsao_tempoTests
+@implementation RecardbusTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
